7036ae35f5574d20c4c0e1278e2a68ce78a0128b
sandeep <sandeep@darkhorsesports.in>
2018-09-10 12:53:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ionic2-rest-authentication
+This source code is part of [Step by step tutorial building ionic 2 rest api authentication](https://www.djamware.com/post/58c1703e80aca7585c808ec1/step-by-step-tutorial-building-ionic-2-rest-api-authentication).
+
+To run locally:
+
+* Clone this repo
+* Run npm install
+* Run ionic serve -l
+
+If you think this source code is useful, it will be great if you just give it star or just buy me a cup of cofee [![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Q5WK24UVWUGBN)
---
+++ src/providers/auth-service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+let apiUrl = 'http://192.168.1.11/blog/public/user_register';
+
+@Injectable()
+export class AuthService {
+
+  constructor(public http: Http) {}
+
+  login(credentials) {
+    return new Promise((resolve, reject) => {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+
+        this.http.post(apiUrl+'login', JSON.stringify(credentials), {headers: headers})
+          .subscribe(res => {
+            resolve(res.json());
+          }, (err) => {
+            reject(err);
+          });
+    });
+  }
+
+  register(data) {
+  console.log(data);
+    return new Promise((resolve, reject) => {
+        
+      
+
+
+    this.http.post(apiUrl, JSON.stringify(data))
+          .subscribe(res => {
+            resolve(res.json());
+          }, (err) => {
+            reject(err);
+          });
+    });
+  }
+
+  logout(){
+    return new Promise((resolve, reject) => {
+        let headers = new Headers();
+        headers.append('X-Auth-Token', localStorage.getItem('token'));
+
+        this.http.post(apiUrl+'logout', {}, {headers: headers})
+          .subscribe(res => {
+            localStorage.clear();
+          }, (err) => {
+            reject(err);
+          });
+    });
+  }
+
+}
